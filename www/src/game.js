eb82f73d67143d48b41f20cd8cd109924ce41a39
LizaSh <lizash605@gmail.com>
2016-09-16 21:40:13 +0300

    Realize Snake head

--- www/src/game.js
+++ www/src/game.js
@@ -9,3 +9,30 @@ var GameScene  = cc.Scene.extend({
     this.addChild(this.snake_layer, 0);
     }
 });
+
+//описания одного сегмента змейки
+var SnakePart = cc.Sprite.extend({
+    ctor: function(sprite) {
+        /* Вызов конструктора суперкласса с передачей ему спрайта, представляющего фрагмент тела змеи */
+        this._super(sprite);
+    },
+    move: function(posX, posY) {
+        /* Обновляем текущую позицию */
+        this.x = posX;
+        this.y = posY;
+    },
+});
+
+var SnakeLayer = cc.Layer.extend({
+    snakeHead: null,
+    ctor: function () {
+         /* Вызов конструктора суперкласса */
+        this._super();
+        
+        /* Создание головы змеи */
+        this.snakeHead = new SnakePart(asset.SnakeHead_png);
+        /* Добавление головы в качестве объекта-потомка слоя */
+        this.addChild(this.snakeHead);
+        
+    },
+});
